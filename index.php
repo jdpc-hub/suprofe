25d54777be37b72b5ae245fc8d7f4c6de7af9a22
jdpc-hub <jdpc20392@gmail.com>
2021-06-16 10:07:49 -0500

    Se agrega la política de privacidad al controlador y la acción a realizar para el formulario de la página de inicio

--- index.php
+++ index.php
@@ -6,6 +6,12 @@ if (isset($_GET["action"])) {
         require_once 'vista/html/registro.php';
     } elseif ($_GET["action"] == "login") {
         require_once 'vista/html/login.php';
+    } elseif ($_GET["action"] == "politica_privacidad") {
+        require_once 'vista/html/politicaPrivacidad.php';
+    }
+} elseif (isset($_POST["action"])) {
+    if ($_POST["action"] == "guardarExterna") {
+        echo "Guardando";
     }
 } else {
     require_once 'vista/html/inicio.php';
